4bef18c44e16986a5dd749bccf8992bddbefbf3d
henry321yu <henry321yu@gmail.com>
2024-09-23 21:16:13 +0800

    wfh

--- ts.py
+++ ts.py
@@ -4,8 +4,8 @@ import os
 
 current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 print("current time :", current_datetime)
-log_folder = r'C:\Users\sgrc-325\Desktop\py\log'
-# log_folder = r'C:\Users\弘銘\Desktop\WFH\git\log'
+# log_folder = r'C:\Users\sgrc-325\Desktop\py\log'
+log_folder = r'C:\Users\弘銘\Desktop\WFH\git\log'
 # log_folder = r'/home/rasp3/Desktop/log'
 log_filename = f'logger_T_{current_datetime}.csv'
 log_filepath = os.path.join(log_folder, log_filename)
