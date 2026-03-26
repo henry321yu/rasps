ef1d08ae320ea28e2936ab0b169649fdafe55ef5
hey <46470927+henry321yu@users.noreply.github.com>
2026-03-26 17:32:41 +0800

    Update read485.py

--- read485.py
+++ read485.py
@@ -1,25 +1,29 @@
-from pymodbus.client import ModbusSerialClient
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-client = ModbusSerialClient(
-    port='COM8',        # 改成你的 COM
+client = ModbusClient(
+    port='COM8',
     baudrate=9600,
-    timeout=1,
     parity='N',
     stopbits=1,
-    bytesize=8
+    bytesize=8,
+    timeout=3
 )
+client.connect()
 
 if not client.connect():
     print("連線失敗")
     exit()
 
-result = client.read_holding_registers(address=0, count=1, device_id=1)
+slave_id = 0x01
 
-if not result.isError():
-    raw = result.registers[0]
-    humidity = raw / 10.0
-    print(f"濕度: {humidity}%")
-else:
-    print("讀取失敗:", result)
+try:
+    result = client.read_holding_registers(address=0, count=2, unit=slave_id)
 
-client.close()
+    if result.isError():
+        print("讀取失敗")
+    else:
+        temperature = result.registers[0] / 10.0
+        humidity = result.registers[1] / 10.0
+        print(f"溫度: {temperature} °C, 濕度: {humidity} %")
+finally:
+    client.close()
